048781a07db617c1e390a71e73c0b93db1db88d9
Rogsu <gonzalo.marolda@gmail.com>
2025-03-06 22:46:37 -0300

    adding front

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('body', (request) => request.method === 'POST' ? JSON.stringify(request.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
