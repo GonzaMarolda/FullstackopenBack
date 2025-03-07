2db000fe47409cbd64fba9a9390c484ebc86fc24
Rogsu <gonzalo.marolda@gmail.com>
2025-03-06 22:28:58 -0300

    backend

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+const cors = require('cors')
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+morgan.token('body', (request) => request.method === 'POST' ? JSON.stringify(request.body) : '')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const info = "Phonebook has info for " + persons.length + " people <br/>"
+    response.send(info + Date())
+  })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    person ? response.json(person) : response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random()*100000)
+  }
+
+  if (!person.name || !person.number) {
+    response.status(400)
+    response.send("Person data is incomplete")
+    return
+  }
+  if (persons.find(p => p.name === person.name)) {
+    response.status(400)
+    response.send("Name must be unique")
+    return
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
