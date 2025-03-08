4c40ca9b7af0c9bd8acad4e6c61649794dcf49eb
Rogsu <gonzalo.marolda@gmail.com>
2025-03-07 23:04:15 -0300

    Updated production

--- models/person.js
+++ models/person.js
@@ -14,10 +14,18 @@ mongoose.connect(url).then(result => {
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
+    minLength: 3,
     required: true
   },
   number: {
     type: String,
+    minLength: 8,
+    validate: {
+        validator: (v) => {
+            return /^\d{2,3}-\d{5,}$/.test(v);
+        },
+        message: props => props.value + " is not a valid phone number"
+    },
     required: true
   },
 })
